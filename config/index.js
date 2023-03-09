736d6b2c19be6c92e1587f1dce6ceb5267aff79d
sheetor <canalescatalandiego@gmail.com>
2023-03-10 00:00:46 +0100

    Update index.js
    just testing

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https//yourwebsite.com'
+export const server = dev ? 'http://localhost:3000' : 'https://exjobb-video-comparison-site-kjvmvv7b8-sheetor.vercel.app/'
