c10bb2e69245312aaa7b92ac941345765e2a376c
sheetor <canalescatalandiego@gmail.com>
2023-03-10 17:52:14 +0100

    new next.config.js
    testing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://exjobb-video-comparison-site-git-db-online-maybe-sheetor.vercel.app/:path*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
