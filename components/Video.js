9714937942087156a5e4ab6911f4940f96d6e557
sheetor <canalescatalandiego@gmail.com>
2023-05-09 13:56:31 +0200

    videos autoplay

--- components/Video.js
+++ components/Video.js
@@ -3,7 +3,7 @@ import React from "react";
 const VideoPage = ({ video }) => {
   return (
     <>
-      <video controls loop muted style={{ width: "auto", height: "425px" }}>
+      <video controls autoPlay loop muted style={{ width: "auto", height: "425px" }}>
         <source src={`${video.url}`} />
       </video>
     </>
